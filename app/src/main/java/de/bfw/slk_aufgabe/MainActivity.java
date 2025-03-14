211f921c282d2beacf2084d1064cb8c04a4c779b
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-14 11:11:01 +0100

    Upload project

---
+++ app/src/main/java/de/bfw/slk_aufgabe/MainActivity.java
@@ -0,0 +1,18 @@
+package de.bfw.slk_aufgabe;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slk1_layout);
+    }
+}
