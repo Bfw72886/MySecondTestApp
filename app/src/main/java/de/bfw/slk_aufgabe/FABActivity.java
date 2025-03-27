1ea1a3cc1d31091e6c5ff3ef696490398c24f171
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-27 10:50:41 +0100

    Add new icon color to FAB

--- app/src/main/java/de/bfw/slk_aufgabe/FABActivity.java
+++ app/src/main/java/de/bfw/slk_aufgabe/FABActivity.java
@@ -13,6 +13,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+/**
+ * erstellt am 27.03.2025
+ *
+ * @author t07
+ *
+ * Diese Klasse enthält die Logik für den FAB
+ */
 public class FABActivity extends AppCompatActivity implements View.OnClickListener {
 
     FloatingActionButton fab;
@@ -33,5 +40,15 @@ public class FABActivity extends AppCompatActivity implements View.OnClickListen
             Log.i("FAB", "geklickt");
             Toast.makeText(getApplicationContext(), "FAB GEKLICKT WOOOOO", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    /**
+     * ABC gibt einen nicht zusammengesetzten String zurück
+     * @param a Name des Autors
+     * @param b ID des Autors
+     * @return zusammengesetzer String aus a und b
+     */
+    public String getABC(String a, int b) {
+        return a + b;
     }
 }
